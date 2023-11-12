d72f08f36d8557608c1c827d2bedb94976dd6774
quanvo <quanminhvo@gmail.com>
2023-11-12 18:14:00 +0700

    Create AutomobilesFullTypeConstants.cs

---
+++ LianAgentPortal/Commons/Constants/AutomobilesFullTypeConstants.cs
@@ -0,0 +1,19 @@
+﻿namespace LianAgentPortal.Commons.Constants
+{
+    public static class AutomobilesFullTypeConstants
+    {
+        public static List<AutomobilesFullTypeObject> Data = new List<AutomobilesFullTypeObject>()
+        {
+            new AutomobilesFullTypeObject() { DisplayName = "", AutomobileType = AutomobileTypeEnum.BUS, Attributes_Seat = "1", Attributes_Category = AutomobileTypeCategoryEnum.COMMERCIAL },
+        };
+    }
+
+    public class AutomobilesFullTypeObject
+    {
+        public string DisplayName { get; set; }
+        public AutomobileTypeEnum AutomobileType { get; set; }
+        public string Attributes_Seat { get; set; }
+        public AutomobileTypeCategoryEnum Attributes_Category { get; set; }
+    }
+
+}
